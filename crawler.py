2c56c2f1052047c15dc3c58e1b0d0ac98afb9878
wjazienicki <wojtekjaz@gmail.com>
2020-03-05 14:04:26 +0000

    separated authentication

--- crawler.py
+++ crawler.py
@@ -2,8 +2,16 @@ import credentials
 
 import tweepy
 
+
+class Authenticator():
+    def authenticate_twitter(self):
+        auth = tweepy.OAuthHandler(credentials.CONSUMER_KEY,credentials.CONSUMER_KEY_SECRET)
+        auth.set_access_token(credentials.ACCESS_TOKEN, credentials.ACCESS_TOKEN_SECRET)
+
+        return auth
+    
  
-class StdOutListener(tweepy.StreamListener):
+class TwitterListener(tweepy.StreamListener):
     
 
 
@@ -23,20 +31,18 @@ class StdOutListener(tweepy.StreamListener):
 
 class Streamer():
     def __init__(self):
-        pass
+        self.authenticator = Authenticator()
     def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
-        listener = StdOutListener()
-        auth = tweepy.OAuthHandler(credentials.CONSUMER_KEY,credentials.CONSUMER_KEY_SECRET)
-        auth.set_access_token(credentials.ACCESS_TOKEN, credentials.ACCESS_TOKEN_SECRET)
-
-        stream = tweepy.Stream(auth, listener)
+        listener = TwitterListener()
+        auth = self.authenticator.authenticate_twitter()
+        stream = tweepy.Stream(auth,  listener)
         stream.filter(track=hash_tag_list)
 
 
 
 if __name__ == '__main__':
-    hash_tag_list = [""]
-    fetched_tweets_filename = "tweets.json"
+    hash_tag_list = ["nba"]
+    fetched_tweets_filename = "tweets_streaming.json"
 
     streamer = Streamer()
     streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
